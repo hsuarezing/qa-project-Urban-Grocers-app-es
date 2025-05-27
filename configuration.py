abd15b8c7240525733c93b4c48ba40b111832430
hsuarezing <hsuarezing@gmail.com>
2025-05-27 18:14:05 -0500

    Termine el proyecto

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-fafac44a-dab8-4c4f-8e4a-c3647c254fe2.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-efb86776-c5f9-4c5f-85cd-33f34e832d0a.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
